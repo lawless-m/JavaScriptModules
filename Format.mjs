f49039b27f894cbe8030a678c22fc4a759e5be2e
matthew.heath <matthew.heath@wrenkitchens.com>
2021-12-02 10:31:22 +0000

    Math!

--- Format.mjs
+++ Format.mjs
@@ -5,6 +5,6 @@ export function pc(f) { return `${f}%`; }
 export function comma(num) { return num.toLocaleString('en-GB'); }
 export function dp(num, digits) { 
     let scale = digits===undefined ? 1 : 10 ^ digits;
-    return round(scale*num)/scale;
+    return Math.round(scale*num)/scale;
 }
 export function div(n, d) { return d > 0 ? Math.round(n / d) : 0; }
