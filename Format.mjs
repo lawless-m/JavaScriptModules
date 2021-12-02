1e9e0cdab93ed8c380e5722facc8115604d4974e
matthew.heath <matthew.heath@wrenkitchens.com>
2021-12-02 11:21:03 +0000

    make it work

--- Format.mjs
+++ Format.mjs
@@ -4,7 +4,7 @@ export function float(txt) { try { return parseFloat(txt, 10); } catch(e) {}; re
 export function pc(f) { return `${f}%`; }
 export function comma(num) { return num.toLocaleString('en-GB'); }
 export function dp(num, digits) { 
-    let scale = digits===undefined ? 1 : 10 ^ digits;
+    let scale = digits===undefined ? 1 : Math.pow(10, digits);
     return Math.round(scale*num)/scale;
 }
-export function div(n, d) { return d > 0 ? Math.round(n / d) : 0; }
+export function div(n, d) { return d > 0 ? n / d : 0; }
