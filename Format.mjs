a5819506f1dd7624efd0f35e365ea1bb3ab6e7e0
matthew.heath <matthew.heath@wrenkitchens.com>
2021-12-02 09:56:11 +0000

    added Format

---
+++ Format.mjs
@@ -0,0 +1,9 @@
+
+export function int(txt) { try { return parseInt(txt, 10); } catch(e) {}; return 0;}
+export function float(txt) { try { return parseFloat(txt, 10); } catch(e) {}; return 0;}
+export function pc(f) { return `${f}%`; }
+export function comma(num) { return num.toLocaleString('en-GB'); }
+export function dp(num, digits) { 
+    let scale = digits===undefined ? 1 : 10 ^ digits;
+    return round(scale*num)/scale;
+}
