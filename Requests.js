33c8f199e2702818234d31b0c20b3bd27c098abb
lesslaw-m <matt-github@steponnopets.net>
2023-06-05 10:02:58 +0100

    add reqeusts

---
+++ Requests.js
@@ -0,0 +1,21 @@
+export async function Post(url, datatype, data, callback) {
+  fetch(url, {
+      method: 'POST',
+      body: data,
+      headers: {'Content-Type': datatype}
+  })
+  .then(response => response.json())
+  .then(data => { if(callback) { callback(data); }});
+}
+
+export async plainPost(url, data, callback) {
+	Post(url, 'text/plain', data, callback);
+}
+
+export async function jsonPost(url, data, callback) {
+    Post(url, 'application/json', JSON.stringify(data), callback);
+}
+
+
+
+
