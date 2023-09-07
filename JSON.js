e6e712bc5bdd1fc9a466e261b0926e80f3428340
matthew.heath <matthew.heath@ramsden-international.com>
2023-09-07 17:21:38 +0100

    gave the urls names

--- JSON.js
+++ JSON.js
@@ -43,3 +43,29 @@ export function downloadBlob(filename, content, contenttype) {
   pom.setAttribute('download', filename);
   pom.click();
 }
+
+export function fetchJsonUrls(urls, callback) {
+  /*
+    urls is an array of objects { u1:url1, u2:url2, u3:url3 }
+
+    returns an object containing data
+
+    { u1:data1, u2:data2, u3:data3 }
+
+  */
+  Promise.all(Object.keys(urls).map(name => {
+    return fetch(urls[name])
+      .then(res => res.json())
+      .then(data => ({ [name]: data }))
+  })).then(results => { 
+  
+    callback(results.reduce((obj, item) => {
+      Object.keys(item).forEach(key => {
+        obj[key] = item[key]; 
+      });
+    
+      return obj;
+    }, {}));
+  
+  })
+}
