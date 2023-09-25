de257e847b051093c328f2f94d1b90d286bd9248
matthew.heath <matthew.heath@ramsden-international.com>
2023-09-25 16:24:17 +0100

    gpt'd

--- Format.js
+++ Format.js
@@ -9,35 +9,21 @@ export function float_eq(a, b, threshold = 0.01) {
 }
 
 export function pcdp(f, digits) {
-    if(f == 1) {
-        return [1,'100%'];
-    }
     let p = dp(100 * f, digits);
-    let t = `${p}`;
-    if(digits == 0) {
-        return [f, `${p}%`];
-    }
-    let zeros = '0'.repeat(digits);
-    let dot = t.indexOf('.');
-    if(dot < 0) {
-        return [f, `${t}.${zeros}%`];
-    }
-    return [f, t.substring(0, dot) + '.' + (t.substring(dot+1) + zeros).substring(0,digits) + '%'];
+    return [f, `${p.toFixed(digits)}%`];
 }
 
 export function pc2(n, d) {
-    if(n == undefined || d == undefined) { return [0,'0.0%']; }
-    if(n == 0 && d == 0) { return [0,'0.0%']; }
+    if(!n || !d || (n == 0 && d == 0)) { return [0,'0.0%']; }
     return pcdp(fdiv(n,d), 1);
 }
 
 export function fixed(t, digits) {
-    let zeros = '0'.repeat(digits);
     let dot = t.indexOf('.');
     if(dot < 0) {
-        return `${t}.${zeros}`;
+        return `${t}.${'0'.repeat(digits)}`;
     }
-    return t.substring(0, dot) + '.' + (t.substring(dot+1) + zeros).substring(0,digits);
+    return t.substring(0, dot) + '.' + (t.substring(dot+1) + '0'.repeat(digits)).substring(0,digits);
 }
 
 export function fixed2(t) { return fixed(t, 2); }
@@ -54,26 +40,17 @@ export function dp(num, digits) {
 }
 
 export function fdiv(n, d) { 
-    switch(true) {
-        case isNaN(n):
-        case isNaN(d):
-        case d == 0:
-            return 0;
-        default:
-            return n / d; 
+    if(isNaN(n) || isNaN(d) || d == 0) {
+        return 0;
     }
+    return n / d;
 }
 
 export function mul(a, b) {
-    switch(true) {
-        case isNaN(a):
-        case isNaN(b):
-        case a == undefined:
-        case b == undefined:
-            return 0;
-        default:
-            return a * b;
+    if(isNaN(a) || isNaN(b) || a == undefined || b == undefined) {
+        return 0;
     }
+    return a * b;
 
 }
 
